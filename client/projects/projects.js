a57e1b5f21d7f878ad63f56968e4356895caedad
gird <greg@gird.us>
2014-08-15 19:29:00 -0400

    UI changes to accordion

--- client/projects/projects.js
+++ client/projects/projects.js
@@ -1,28 +1,36 @@
 Template.projects.helpers({
-  projects: function() {
-    return Projects.find();
-  },
-  waitOn: function() {
-    Meteor.subscribe('projects');
-  }
+    projects: function () {
+        return Projects.find();
+    },
+    waitOn: function () {
+        Meteor.subscribe('projects');
+    }
 });
 
 Template.projectView.helpers({
-  'company': function() {
-     return company = Companies.findOne(this.company_id).name;
-  },
-  'rateBook': function() {
-     return rateBook = Rate_Books.findOne(this.rate_book_id).name;
-  },
-  projectRoles: function() {
-    var projectRoles = Project_Roles.find({ project_id: this._id });
-    projectRoles.forEach(function(projectRole) {
-      var roleId = Rate_Book_Roles.findOne({_id:projectRole.rate_book_role_id}).role_id;
-      var role = Roles.findOne({_id: roleId}); 
-    })
-    return projectRoles;
-  },
-  projectOpportunities: function() {
-    return projectOpportunities = Opportunities.find({ project_id: this._id });
-  }
+    'company': function () {
+        return company = Companies.findOne(this.company_id).name;
+    },
+    'rateBook': function () {
+        return rateBook = Rate_Books.findOne(this.rate_book_id).name;
+    },
+    projectRoles: function () {
+        var projectRoles = Project_Roles.find({
+            project_id: this._id
+        });
+        projectRoles.forEach(function (projectRole) {
+            var roleId = Rate_Book_Roles.findOne({
+                _id: projectRole.rate_book_role_id
+            }).role_id;
+            var role = Roles.findOne({
+                _id: roleId
+            });
+        })
+        return projectRoles;
+    },
+    projectOpportunities: function () {
+        return projectOpportunities = Opportunities.find({
+            project_id: this._id
+        });
+    }
 });
