42ef178acc2302d0c48fa307abc2c3c3ccb6ce13
Greg Kenawell <greg@gird.us>
2014-08-20 01:43:40 -0400

    small updates

--- client/opportunities/opportunities.js
+++ client/opportunities/opportunities.js
@@ -1,6 +1,11 @@
 Template.opportunities.helpers({
   opportunities: function() {
-    return Opportunities.find();
+    //return Opportunities.find();
+    var opportunities = Opportunities.find();
+    opportunities.forEach(function(opportunity) {
+        var company = Companies.findOne({_id:opportunity.company_id}).name;
+    });
+    return opportunities;
   }
 });
 
