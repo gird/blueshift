f3e324faaf34342ea353731ee4252da8b8a6d4da
Greg Kenawell <greg@gird.us>
2014-08-21 00:37:45 -0400

    Added insert/remove functions for Roles

--- client/roles/roles.js
+++ client/roles/roles.js
@@ -3,3 +3,26 @@ Template.roles.helpers({
     return Roles.find();
   }
 });
+
+Template.newRoleRow.events({
+    
+    /*
+    'click .role_name':function() {
+        $('.role_form').toggle();
+    },
+    */
+    'click .submit_role': function () {
+        var roleName = $('.role_name');
+        Roles.insert({
+            name: roleName.val()
+        });
+        roleName.val('');
+    }
+});
+
+Template.roleListRow.events({
+    'click a.delete_role': function (e) {
+        e.preventDefault();
+        Roles.remove(this._id);
+    }
+});
