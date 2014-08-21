da258755cba69047d0431b7d1e3171970854dd32
Greg Kenawell <greg@gird.us>
2014-08-21 12:15:56 -0400

    Updated roles page to allow for editing, still need to fix submit_edit_name function.

--- client/roles/roles.js
+++ client/roles/roles.js
@@ -4,6 +4,8 @@ Template.roles.helpers({
   }
 });
 
+Session.set('editing_rolename', null);
+
 Template.newRoleRow.events({
     
     /*
@@ -20,9 +22,41 @@ Template.newRoleRow.events({
     }
 });
 
+var focus_field_by_class = function (className) {
+  var input = document.getElementsByClassName(className);
+  if (input) {
+    input.focus();
+    input.select();
+  }
+  if (li) {
+    li.focus();
+  }
+};
+
+Template.roleListRow.editing = function () {
+  return Session.equals('editing_rolename', this._id);
+};
+
 Template.roleListRow.events({
     'click a.delete_role': function (e) {
         e.preventDefault();
         Roles.remove(this._id);
+    },
+    'click a.edit_role': function () {
+        Session.set('editing_rolename', this._id);
+        Meteor.flush(); // update DOM before focus
+        focus_field_by_class("edit_role_name");
+    }
+});
+
+Template.editRoleRow.events({
+    'click a.submit_edit_role': function () {
+        var roleName = $('.edit_role_name');
+        Roles.update(
+            this._id, {$set: {name: roleName.val()}}
+        );
+        roleName.val('');
+        Session.set('editing_rolename', '');
+        focus_field_by_class("");
     }
 });
