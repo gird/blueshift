0f1212f26e26428eb520fe2f60abaa3e5d98b3a2
gird <greg@gird.us>
2014-08-26 00:46:59 -0400

    added ability to add rate book roles to a rate book using a modal window. Still need to close the modal on submit.

--- client/rate books/viewRateBook.js
+++ client/rate books/viewRateBook.js
@@ -19,3 +19,21 @@ Template.rateBookRelated_projects.helpers({
     return rateBookProjects = Projects.find({rate_book_id: this._id});
   }
 });
+
+Template.rateBookRolesButtons.helpers({
+    roles: function() {
+        return Roles.find();
+    }
+});
+
+Template.rateBookRolesButtons.events({
+    'click .submit_new_ratebookrole': function() {
+        var roleId = $('.ratebookrole_roleid').val();
+        var rate = $('.ratebookrole_rate').val();
+        Rate_Book_Roles.insert({
+            rate_book_id: this._id,
+            role_id: roleId,
+            rate: rate
+        });
+    }
+});
