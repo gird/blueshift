f2d2bf7ae57adb823278371dc7f8d409e343bb06
gird <greg@gird.us>
2014-09-24 01:21:58 -0400

    added assigned/claimed resourses to the project role view and updated some accordion settings in order to allow mulitple groups be opened at once.

--- client/project roles/projectRoles.js
+++ client/project roles/projectRoles.js
@@ -69,3 +69,19 @@ Template.newProjectRoleSkillModal.events({
         Session.set('adding_ratebookrole_rate', null);
     }*/
 });
+
+Template.projectRoleRelated_resources.helpers({
+    resources: function () {
+        return this.resources;
+    }
+});
+
+Template.relatedResourcesListRow.helpers({
+    'resourceName': function () {
+        var resource = Resources.findOne({
+            _id: this.resource_id
+        });
+        var resourcename = resource && (resource.firstname + ' ' + resource.lastname);
+        return resourcename;
+    }
+});
