064c44d7943e21f185f772bde3d9b780e3dfec0f
Greg Kenawell <greg@gird.us>
2014-09-30 18:01:14 -0400

    changed accounts-ui to accounts-entry, added some resource skill functionality

--- client/navigation/navigation.js
+++ client/navigation/navigation.js
@@ -1,9 +1,9 @@
 Template.navigation.rendered = function () {
-  $(function(){ 
-     var navMain = $("#navbarCollapse");
+    $(function(){ 
+        var navMain = $("#navbarCollapse");
 
-     navMain.on("click", "a", null, function () {
-         navMain.collapse('hide');
-     });
- });
+        navMain.on("click", "a", null, function () {
+            navMain.collapse('hide');
+        });
+    });
 }
