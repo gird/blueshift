81b1682d79b6394682e45534a67474298407ba2e
Greg Kenawell <greg@gird.us>
2014-11-06 00:14:12 -0500

    figured out the typeahead template issue on the create new opportunity workflow

---
+++ client/opportunities/listOpportunities.js
@@ -0,0 +1,52 @@
+Template.opportunities.helpers({
+    opportunities: function () {
+        return opportunities = Opportunities.find();
+    }
+});
+
+Template.opportunityListRow.helpers({
+    'represented_amount': function () {
+        return accounting.formatMoney(this.represented_amount());
+    },
+    'estimated_amount': function () {
+        return accounting.formatMoney(this.estimated_amount);
+    }
+});
+
+
+Template.newOpportunityExistingProjectModal.rendered = function () {
+    Meteor.typeahead.inject();
+    var projects = Projects.find().fetch();
+
+    $('.newOpportunity_ProjectCompany').on('typeahead:selected', function (evt, item) {
+        console.log(item.valueKey);
+    });
+}
+
+Template.newOpportunityExistingProjectModal.helpers({
+    'companyProjectLookup': function () {
+        var projects = Projects.find();
+        var data = [];
+        projects.forEach(function (project) {
+            var projectName = project.name;
+            var projectId = project._id
+            var company = Companies.findOne(project.company_id);
+            var companyName = company && company.name;
+            var companyId = company && company._id;
+            data.push({
+                "name" : projectName + " - " + companyName,
+                "description": "test",
+                "language": "testlan",
+                "value": projectName + " " + companyName,
+                "valueKey": projectId,
+                "tokens": [
+                  projectName,
+                  companyName
+                ]
+            });
+        });
+        console.log(projects);
+        return data;
+    }
+
+});
